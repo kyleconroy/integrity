dd5a111e3ac2a8bb74287f30f02b229e482d42a5
Kyle Conroy <kyle@kyleconroy.com>
2017-07-29 04:18:25 -0700

    Initial implementation

---
+++ integrity.go
@@ -0,0 +1,79 @@
+package sri
+
+import (
+	"crypto/sha512"
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+)
+
+type FileSet struct {
+	// A mapping of path name to SHA512 hash
+	hashes map[string]string
+	root   string
+}
+
+// We're not actually parsing anything
+func ParseFiles(root string) (*FileSet, error) {
+	hashes := map[string]string{}
+	return &FileSet{hashes, root}, filepath.Walk(root, func(p string, info os.FileInfo, other error) error {
+		if other != nil {
+			return other
+		}
+		if strings.HasPrefix(info.Name(), ".") {
+			return nil
+		}
+		if info.IsDir() {
+			return nil
+		}
+		data, err := ioutil.ReadFile(p)
+		if err != nil {
+			return err
+		}
+		hash := sha512.New()
+		hash.Write([]byte(data))
+		hashes[p] = "sha512-" + base64.StdEncoding.EncodeToString(hash.Sum(nil))
+		return nil
+	})
+}
+
+func (b *FileSet) Digest(p string) (string, error) {
+	fp := filepath.Join(b.root, filepath.FromSlash(path.Clean("/"+p)))
+	if h, ok := b.hashes[fp]; ok {
+		return h, nil
+	}
+	return "", fmt.Errorf("no integrity value for subresource %s", p)
+}
+
+type Verifier struct {
+	Param    string
+	NotFound http.Handler
+
+	bundle *FileSet
+	next   http.Handler
+}
+
+func (v *Verifier) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	values := r.URL.Query()
+	if sha := values.Get(v.Param); sha != "" {
+		if integ, _ := v.bundle.Digest(r.URL.Path); integ != sha {
+			v.NotFound.ServeHTTP(w, r)
+			return
+		}
+	}
+	v.next.ServeHTTP(w, r)
+}
+
+func Verify(b *FileSet, next http.Handler) *Verifier {
+	return &Verifier{
+		next:     next,
+		bundle:   b,
+		NotFound: http.NotFoundHandler(),
+		Param:    "sha",
+	}
+}
---
+++ integrity_test.go
@@ -0,0 +1,75 @@
+package sri
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"path/filepath"
+	"testing"
+)
+
+const (
+	DigestCSS = "sha512-tXj6GsgkfpVKb1u4ktHSdZAn3B3CEqvcv4OgB2zlCQ2MFMYa/k8qc6JaS0BTe5kTpvs8ywuhGMsqyzzNEowyFA=="
+	DigestJS  = "sha512-/9UYockPRzjO73c05tr9/jrzEbKGVSZzyk+rrt1e7siZS6IXlbjZYCegSetVVQ+fIdmIIfHbpgXVkJHuFxE8Jg=="
+	DigestJPG = "sha512-AJMABkkop36C2qXMAsP6xFWjW2r7Q7M8bd6/1U1m+rg3ANlrcRewBZzfS15J3NtYvHBdnrWuYYCDyjaaUduzxQ=="
+)
+
+func TestDigests(t *testing.T) {
+	fs, err := ParseFiles("testdata")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for path, digest := range map[string]string{
+		"css/style.css":        DigestCSS,
+		"js/godocs.js":         DigestJS,
+		"img/fancygopher.jpg":  DigestJPG,
+		"/css/style.css":       DigestCSS,
+		"/js/godocs.js":        DigestJS,
+		"/img/fancygopher.jpg": DigestJPG,
+	} {
+		t.Run(path, func(t *testing.T) {
+			d, err := fs.Digest(path)
+			if err != nil {
+				t.Error(err)
+			}
+			if d != digest {
+				t.Errorf("Mismatched digest: %s != %s", digest, d)
+			}
+		})
+	}
+}
+
+func TestVerification(t *testing.T) {
+	fs, err := ParseFiles("testdata")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	h := Verify(fs, http.FileServer(http.Dir("testdata")))
+	h.Param = "h"
+	handler := http.StripPrefix("/static/", h)
+
+	js := url.Values{"h": []string{DigestJS}}
+	css := url.Values{"h": []string{DigestCSS}}
+
+	for _, pair := range []struct {
+		req  *http.Request
+		code int
+	}{
+		{httptest.NewRequest("GET", "/static/css/style.css", nil), 200},
+		{httptest.NewRequest("GET", "/static/css/style.css?h=", nil), 200},
+		{httptest.NewRequest("GET", "/static/css/style.css?"+css.Encode(), nil), 200},
+		{httptest.NewRequest("GET", "/static/css/style.css?h=invalid", nil), 404},
+		{httptest.NewRequest("GET", "/static/css/style.css?"+js.Encode(), nil), 404},
+	} {
+		t.Run(filepath.Base(pair.req.URL.String()), func(t *testing.T) {
+			w := httptest.NewRecorder()
+			handler.ServeHTTP(w, pair.req)
+			resp := w.Result()
+			if resp.StatusCode != pair.code {
+				t.Errorf("Expected status code of %d, not %d", pair.code, resp.StatusCode)
+			}
+		})
+	}
+}
